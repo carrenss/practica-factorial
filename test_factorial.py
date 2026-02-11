cbee70e158198fdcea038fec56a69f3e916ffcc4
carrenss <alvacarre56@gmail.com>
2026-02-11 12:27:14 +0100

    forzando error para prueba

--- test_factorial.py
+++ test_factorial.py
@@ -5,8 +5,7 @@ from factorial import factorial
 def test_factorial_basico():
     """Prueba valores estándar."""
     assert factorial(3) == 6
-    assert factorial(5) == 120
-
+    assert factorial(5) == 500
 def test_factorial_cero_y_uno():
     """Prueba casos base."""
     assert factorial(0) == 1
